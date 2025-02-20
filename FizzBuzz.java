648d23337ebb2f32a02a3a27ea2f754ed09edbaa
MvhS-code <melissavanhetspijker@gmail.com>
2025-02-20 15:20:21 +0100

    Update FizzBuzz.java
    Adding Fizzbuzz print and comments

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,11 +2,18 @@ class FizzBuzz {
 
     public static void main(String[] args) {
       for (int i = 1; i <= 100; i++){
-        if (i % 3 == 0) {
+        //when i can be divided by 3 and 5
+        if (i % 3 == 0 && i % 5 == 0){
+          System.out.println("FizzBuzz");
+        } 
+        // when i can only be divided by 3
+        else if (i % 3 == 0) {
           System.out.println("Fizz");
         } 
+        //when i can only be divided by 5
         else if (i % 5 == 0) {
           System.out.println("Buzz");
+            
         } else {
           System.out.println(i);
         }
